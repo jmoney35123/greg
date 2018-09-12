fd97d0fa31b25a501ab3412948821a3879b29821
jmoney35123 <javonwilliams786@gmail.com>
2018-09-12 13:47:33 -0400

    adding files

---
+++ src/main/java/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("whats ya name");
+        Scanner Flicia = new Scanner(System.in);
+        String name = Flicia.nextLine();
+        System.out.println("whats ya number");
+        long number = Flicia.nextLong();
+          System.out.println("are you DTF");
+        boolean dtf = Flicia.nextBoolean();
+        if (dtf == true ){
+            System.out.println("lets goooooooooooooo");
+        }
+        else {
+            System.out.println("suck my dick Whooore");
+
+
+
+        }
+    }
+}
+
+
---
+++ src/main/java/javon/Main.java
@@ -0,0 +1,24 @@
+package javon;
+
+import java.util.Scanner;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        //dont type above here
+
+
+
+
+
+
+
+
+
+
+
+        // i should not type below here
+
+}
+}
